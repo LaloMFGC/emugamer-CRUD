25916a834774a4178ef4141aec803f4dd90563be
LaloMFGC <lalomfgc@gmail.com>
2020-05-23 02:31:46 -0500

    Version con Procfile actualizado cleardb

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://b936bb34e99664:50ed6488@us-cdbr-east-06.cleardb.net'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://bd259fd4ca6c2c:b5d58082@us-cdbr-east-06.cleardb.net'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 
 db = SQLAlchemy(app)
